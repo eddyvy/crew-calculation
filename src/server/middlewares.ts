3435fd49f7683c488c661c6b7728bfc680676359
Eduard <edwardyue93@gmail.com>
2021-10-12 16:56:51 +0200

    Cors installed

--- src/server/middlewares.ts
+++ src/server/middlewares.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 
 export const expressMiddlewares = (app: Express): void => {
+  app.use(cors())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
 }
