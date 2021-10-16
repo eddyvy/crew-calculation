3cdb443028489bfa215847614d1a18087698d19b
Eduard <edwardyue93@gmail.com>
2021-10-16 08:48:26 +0200

    Constants tests

---
+++ test/common/constants.test.ts
@@ -0,0 +1,29 @@
+import { DB_ENTITIES, SALT_ROUNDS } from '../../src/common/constants'
+
+describe('Constants', () => {
+
+  test('DB_ENTITES values should be as expected', () => {
+    expect(DB_ENTITIES).toEqual({
+      FLIGHTS: {
+        name: 'flights',
+        uniqueKey: null,
+      },
+      SALARY_TABLES: {
+        name: 'salaryTables',
+        uniqueKey: null,
+      },
+      SCHEDULES: {
+        name: 'schedules',
+        uniqueKey: null,
+      },
+      USERS: {
+        name: 'users',
+        uniqueKey: 'email',
+      },
+    })
+  })
+
+  test('SALT_ROUNDS value should be as expected', () => {
+    expect(SALT_ROUNDS).toBe(10)
+  })
+})
